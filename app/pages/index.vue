3e2a09e7e832e5026319183ff52b36bf6addf90e
Lee Castelani <notayogilee@gmail.com>
2026-02-08 16:20:38 -0500

    Initial commit. setup and created pages.

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <div
+      class="bg-white flex flex-col items-center justify-center w-1/2 h-1/2 p-24 shadow-2xl rounded-lg"
+    >
+      <h1 class="pb-12 text-2xl">Home Page</h1>
+      <h3>Welcome {User}</h3>
+    </div>
+  </div>
+</template>
---
+++ app/pages/registration.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <div
+      class="bg-white flex flex-col items-center justify-center w-1/2 h-1/2 p-24 shadow-2xl rounded-lg"
+    >
+      <h1 class="pb-12 text-2xl">Register</h1>
+      <form class="w-full flex flex-col gap-5" action="">
+        <input
+          type="email"
+          placeholder="email"
+          class="px-3 py-1 border-gray-300 border rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          class="px-3 py-1 border-gray-300 border rounded-lg"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          class="px-3 py-1 border-gray-300 border rounded-lg"
+        />
+        <button
+          class="rounded-lg p-2 mt-4mx-auto bg-red-500 text-bold text-white"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
